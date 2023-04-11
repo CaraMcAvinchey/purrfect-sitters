4f75d00fbf17ef04c88a15506f8bf72f3d950733
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-04-11 14:02:31 +0000

    Add Procfile, update requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn purrfect_sitters.wsgi
--- purrfect_sitters/settings.py
+++ purrfect_sitters/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
-import env
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +78,8 @@ WSGI_APPLICATION = 'purrfect_sitters.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
