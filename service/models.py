c5d0d04522f67189c4a9ab029d8e8de956e65239
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-04 19:16:29 +0000

    Add Service model and views

--- service/models.py
+++ service/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Services(models.Model):
+    """
+    Model for the information about the services on offer.
+    """
+
+    service_name = models.CharField(
+        max_length=100
+    )
+
+    service_description = models.TextField(
+        max_length=300
+    )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
+    SERVICE_CHOICES = [
+        ("1", "Package 1 - 30 minutes"),
+        ("2", "Package 2 - 60 minutes"),
+    ]
+
+    service_choice = models.IntegerField(
+        choices=SERVICE_CHOICES,
+        default=1,
+        null=True
+    )
+
+    def __str__(self):
+        #  Returns a string of name of the service
+        return f"{self.service_name}"
--- service/views.py
+++ service/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Service
 
-# Create your views here.
+
+class Home(generic.TemplateView):
+    model = Service
+    template_name = 'index.html'
+
+
+class ServiceList(generic.ListView):
+    model = Service
+    template_name = 'services.html'
