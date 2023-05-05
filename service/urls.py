3d45404b11dba0677792ffdb37906426da39450f
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-05 21:40:30 +0000

    Add home app, views and urls

--- service/urls.py
+++ service/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.urls import path
-from .views import Home, ServiceList
+from .views import ServiceList
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
     path('services/', ServiceList.as_view(), name='services'),
 ]
--- service/views.py
+++ service/views.py
@@ -3,11 +3,6 @@ from django.views import generic
 from .models import Services
 
 
-class Home(generic.TemplateView):
-    model = Services
-    template_name = 'index.html'
-
-
 class ServiceList(generic.ListView):
     model = Services
     template_name = 'services.html'
