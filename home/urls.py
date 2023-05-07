b9902ec54078d012f75a5fa7aa90b61a073a1faf
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-07 22:33:43 +0000

    Add error 400, 403, 404, 500 pages with styling

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('400', views.Page400.as_view(), name='400'),
+    path('403', views.Page403.as_view(), name='403'),
+    path('404', views.Page404.as_view(), name='404'),
+    path('500', views.Page500.as_view(), name='500'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
 def index(request):
-    """ A view to return the index page """
+    """
+    View that returns the home page.
+    """
 
     return render(request, 'home/index.html')
+
+
+class Page400(TemplateView):
+    """
+    View that returns 400 error page.
+    """
+    template_name = '400.html'
+
+
+class Page403(TemplateView):
+    """
+    View that returns 403 error page.
+    """
+    template_name = '403.html'
+
+
+class Page404(TemplateView):
+    """
+    View that returns 404 error page.
+    """
+    template_name = '404.html'
+
+
+class Page500(TemplateView):
+    """
+    View that returns 500 error page.
+    """
+    template_name = '500.html'
