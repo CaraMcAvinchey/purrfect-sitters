cd5abb82d6f623d51e63b16b268dcce8bde7bb62
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-11 20:47:56 +0000

    Install crispy forms for booking app

--- purrfect_sitters/settings.py
+++ purrfect_sitters/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     # Other
     'storages',
     'phonenumber_field',
+    'crispy_forms',
 
 ]
 
@@ -70,6 +71,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'purrfect_sitters.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -87,6 +90,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
