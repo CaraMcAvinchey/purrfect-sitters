e8fdf3ce0b272a232d02370aa27f15e5c8907d9d
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-12 19:48:04 +0000

    Add settings to urls.py in project

--- purrfect_sitters/urls.py
+++ purrfect_sitters/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,6 +25,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include('service.urls')),
     path('', include('cat.urls')),
-    # path('', include('booking.urls')),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
