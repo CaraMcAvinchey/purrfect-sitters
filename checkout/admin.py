8d400bc5db7aa026768dd6cf63d879c359cfc67c
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-23 19:03:53 +0000

    Add admin.py structure for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Booking, Cat
 
-# Register your models here.
+
+@admin.register(Booking)
+class CheckingAdmin(admin.ModelAdmin):
+
+    readonly_fields = (
+        'owner',
+        'order_number',
+        'payment_date',
+    )
+
+    fields = (
+        'order_number',
+        'payment_status',
+        'street_address1',
+        'town_or_city',
+        'post_code'
+    )
+
+    list_display = [
+        'owner',
+        'order_number',
+        'payment_status',
+        'street_address1',
+        'town_or_city',
+        'post_code'
+    ]
+
+    ordering = ('-payment_date',)
