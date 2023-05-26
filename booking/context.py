a28744e7a7c1e6b8f41a1071c0064537f27ce2c1
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-26 20:22:51 +0000

    Add context processer to booking.py

---
+++ booking/context.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from django.conf import settings
+
+
+def booking_contents(request):
+
+    booking_items = []
+    booking_total = 0
+
+    context = {}
+
+    return context
--- purrfect_sitters/settings.py
+++ purrfect_sitters/settings.py
@@ -92,6 +92,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'booking.contexts.booking_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
