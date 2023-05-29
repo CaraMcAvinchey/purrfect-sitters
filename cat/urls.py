da6dc962e0670685b29c53b222109895d4161863
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-05-29 19:28:19 +0000

    Cat url python validation

--- cat/urls.py
+++ cat/urls.py
@@ -5,9 +5,29 @@ from .views import CatList, CatDetail, CatCreateView, EditCat, DeleteCat
 
 
 urlpatterns = [
-    path('cat/', CatList.as_view(), name='cat'),
-    path('cat/<int:pk>', CatDetail.as_view(), name='cat_detail'),
-    path('cat/add/', login_required(CatCreateView.as_view()), name='add_cat'),
-    path('cat/edit/<int:pk>/', login_required(EditCat.as_view()), name='edit_cat'),
-    path('cat/delete/<int:pk>/', login_required(DeleteCat.as_view()), name='delete_cat'),
+    path(
+        'cat/',
+        CatList.as_view(),
+        name='cat'
+    ),
+    path(
+        'cat/<int:pk>',
+        CatDetail.as_view(),
+        name='cat_detail'
+    ),
+    path(
+        'cat/add/',
+        login_required(CatCreateView.as_view()),
+        name='add_cat'
+    ),
+    path(
+        'cat/edit/<int:pk>/',
+        login_required(EditCat.as_view()),
+        name='edit_cat'
+    ),
+    path(
+        'cat/delete/<int:pk>/',
+        login_required(DeleteCat.as_view()),
+        name='delete_cat'
+    ),
 ]
